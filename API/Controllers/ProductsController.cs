800b0176a73b06f8d737f33db130e50686fbc61c
Guilherme Povoa <20740@alunos.aescas.net>
2026-04-22 09:23:53 +0100

    End of section 13

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,98 @@
+using API.Controllers;
+using API.RequestHelpers;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(
+        [FromQuery] ProductSpecParams specParams)
+    {
+        var spec = new ProductSpecification(specParams);
+
+        return await CreatePageResult(repo, spec, specParams.PageIndex, specParams.PageSize);
+    }
+
+    [HttpGet("{id:int}")] // api/products/2
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await repo.GetByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+
+
+    //Create Products
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+    {
+        repo.Add(product);
+        if(await repo.SaveAllAsync())
+        {
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        return BadRequest("Problem creating theproduct");
+    }
+
+
+
+    //Update Products
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id)) 
+            return BadRequest("Cannot update this product");
+        repo.Update(product);
+        if (await repo.SaveAllAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Problem updating the product");
+    }
+
+
+
+    //Delete Products
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await repo.GetByIdAsync(id);
+        if (product == null) return NotFound();
+        repo.Delete(product);
+        if(await repo.SaveAllAsync())
+        {
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        return BadRequest("Problem deleting theproduct");
+    }
+
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        var spec = new BrandListSpecification();
+        return Ok(await repo.ListAsync(spec));
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        var spec = new TypeListSpecification();
+        return Ok(await repo.ListAsync(spec));
+    }
+
+    private bool ProductExists(int id)
+    {
+        return repo.Exists(id);
+    }
+} 
---
+++ API/Program.cs
@@ -0,0 +1,56 @@
+using API.Middleware;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProductRepository, ProductsRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connString = builder.Configuration.GetConnectionString("Redis") 
+        ?? throw new Exception ("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
+
+var app = builder.Build();
+
+
+//Configure the HTTP request pipeline.
+
+
+app.UseMiddleware<Exception_Middlewear>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200", "http://localhost:4200"));
+
+app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception)
+{
+    
+    throw;
+}
+app.Run();
